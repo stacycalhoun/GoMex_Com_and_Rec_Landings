a89ffcf541d843f780e92853a19b5ecd84407cd8
Stacy <Stacy@Stacys-MacBook-Pro.local>
2019-06-05 12:11:12 -0500

    5 Jun 2019 Improved Code
    Code based on Access queries for Commercial landings were added. Still missing a Step 3a, but the rest of the code is functional. Need to update FishList

--- NOAA NMFS Commercial Landings 1990_2017.R
+++ NOAA NMFS Commercial Landings 1990_2017.R
@@ -7,7 +7,7 @@ assign("last.warning", NULL, envir = baseenv())
 # Libraries ---------------------------------------------------------------
 
 library(plyr)
-library(dplyr)
+#library(dplyr)
 library(tidyverse)
 library(readr)
 library(purrr)
@@ -16,22 +16,65 @@ library(data.table)
 # Reading csvs into one dataframe ---------------------------------------------------------
 
 RawData <-
-  list.files(pattern="*.csv") %>% 
+  list.files(pattern="*_LANDINGS.csv") %>% 
   map_df(~read_csv(.))
 
-
+FishList = read_csv("FisheriesGrpList.csv") #Contains FisheriesGrp column to be added later
 # Wrangling ---------------------------------------------------------------
 
 #Getting rid of value column and freshwater drum
 RawData$`$` = NULL
 
+
 RawData = subset(RawData, RawData$Species != "DRUM, FRESHWATER")
 
-#Adding FisheriesGrp Column
-RawData$FisheriesGrp = NA
-RawData$FisheriesGrp[RawData$Species %like% "AMBERJACK"| RawData$Species %like% "BARRACUDAS" | RawData$Species %like% "BONITO" |
-                       RawData$Species %like% "COBIA" | RawData$Species %like% "JACK" | RawData$Species %like% "MACKEREL" | RawData$Species %like% "POMPANO"] = 'Lg_pelagics'
-RawData$FisheriesGrp[RawData$Species %like% "MENHADEN"] = "Menhaden"
-RawData$FisheriesGrp[RawData$Species %like% "SHARK"] = "Sharks"
-RawData$FisheriesGrp[RawData$Species %like% "BUTTER"] = "Butterfish"
-RawData$FisheriesGrp[RawData$Species %like% "HAKE" | RawData$Species %like% "TRIGGER" | RawData$Species %like% "SCAMP"] = "Lg_demersals"
+
+#Adding FisheriesGrp Column, changing characters to factors and renaming "Metric Tons" column for ease of use
+
+ComFish = merge(RawData, FishList, by = "Species", all.x = T)
+
+ComFish$Species = as.factor(ComFish$Species)
+
+ComFish$Month = as.factor(ComFish$Month)
+
+ComFish$FisheriesGrp = as.factor(ComFish$FisheriesGrp)
+
+ComFish$State = as.factor(ComFish$State)
+
+colnames(ComFish)[5] = "Metric_Tons"
+
+
+
+# Query Chain Steps -------------------------------------------------------
+
+#Step 1: LA-MS-AL commercial landings--setting filters for model domain
+
+Com_Fish_Step1 = subset(ComFish, ComFish$State != "Texas" & ComFish$State !="Florida West Coast")
+
+Com_Fish_Step1 = ddply(Com_Fish_Step1, .(FisheriesGrp, Year, Month, State), summarize,
+                       SumOfMetric_Tons = sum(Metric_Tons)
+                       )
+
+#Steps 2 and 3: LA-MS-AL commercial landings--Step 2: estimate total, annual landings for each functional group; 
+#Step 3: LA-MS-AL commercial landings--estimate annual tonnes/km2 for each functional group
+
+Com_Fish_Step2and3 = ddply(Com_Fish_Step1, .(Year, FisheriesGrp), summarize,
+                           domain = 42130, #This is from the shelf model just to run the code--Need to change!!!
+                       AnnualSum_metric_tons = sum(SumOfMetric_Tons), 
+                       Tonnes_km2 = AnnualSum_metric_tons/domain
+                       )
+
+#Step 3a: LA-MS-AL commercial landings--This steps incorporates inshore/offshore data from a different table. 
+#Space holder to add-in in the future
+
+
+#Step 4: LA-MS-AL commercial landings-- estimating long-term average, total annual landings for each functional group
+
+
+Com_Fish_Step4 = ddply(Com_Fish_Step2and3, .(FisheriesGrp), summarize,
+                       CountTonnes_km2 = length(Tonnes_km2),
+                       AvgTonnes_km2 = mean(Tonnes_km2),
+                       VarTonnes_km2 = var(Tonnes_km2),
+                       StDevTonnes_km2 = sqrt(VarTonnes_km2)
+                       )
+
