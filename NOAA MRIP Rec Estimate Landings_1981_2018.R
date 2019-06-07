096587305b049064925f7c10d636e80cb1fd59d2
Stacy <stacy.calhoun1@louisiana.edugit config --global user.email stacy.calhoun1@louisiana.edugit config --global user.email>
2019-06-07 11:31:19 -0500

    7 June 2019
    Initial Commit
    
    Code to Organize MRIP estimate data for use in the GoMex Model

---
+++ NOAA MRIP Rec Estimate Landings_1981_2018.R
@@ -0,0 +1,38 @@
+#NOAA MRIP Recreational Fisheries Estimate Data Wrangling and Access Query Rewrites
+#Stacy Calhoun, Created 7 June 2019
+
+remove(list=ls())
+assign("last.warning", NULL, envir = baseenv())
+# Libraries ---------------------------------------------------------------
+
+library(plyr)
+library(tidyverse)
+library(readr)
+library(purrr)
+library(data.table)
+
+# Reading MRIP csvs into one dataframe and reading in functional group list ---------------------------------------------------------
+
+RawData <-
+  list.files(pattern="mrip_catch_wave*") %>% 
+  map_df(~read_csv(.))
+
+FishList = read.csv("RecFisheriesGrp.csv", stringsAsFactors = F)
+
+
+# Data Wrangling and Subsetting -------------------------------------------
+
+MRIP = subset(RawData, RawData$st_f == "LOUISIANA" | RawData$st_f == "ALABAMA" | RawData$st_f == "MISSISSIPPI" & RawData$status == 'FINAL', 
+              select = c("status", "year", "wave", "sub_reg", "st_f", "common", "area_x",
+                                  "landing", "land_var", "wgt_ab1", "var_wab1", "miss_fish"))
+
+MRIP_Func = merge(MRIP, FishList, by = "common", all.x = T)
+
+Unwanted = anti_join(MRIP_Func, FishList, by = "common")
+
+Unwanted_Species = unique(Unwanted$common)
+
+MRIP_Func = MRIP_Func[!MRIP_Func$common %in% Unwanted_Species, ]
+
+
+
