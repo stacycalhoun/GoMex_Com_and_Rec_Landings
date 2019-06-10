7e4faf16247e24d857773c8996ac90883eec9c07
Stacy <stacy.calhoun1@louisiana.edugit config --global user.email stacy.calhoun1@louisiana.edugit config --global user.email>
2019-06-10 13:18:38 -0500

    10 June 2019
    Added Query Chains to calculate tonnes harvest per km2.

--- NOAA MRIP Rec Estimate Landings_1981_2018.R
+++ NOAA MRIP Rec Estimate Landings_1981_2018.R
@@ -24,9 +24,10 @@ FishList = read.csv("RecFisheriesGrp.csv", stringsAsFactors = F)
 
 # Data Wrangling and Subsetting -------------------------------------------
 
-MRIP = subset(RawData, RawData$st_f == "LOUISIANA" | RawData$st_f == "ALABAMA" | RawData$st_f == "MISSISSIPPI" & RawData$status == 'FINAL', 
+MRIP = subset(RawData, RawData$status == 'FINAL' & RawData$sub_reg == 7 & RawData$st_f == "LOUISIANA" | RawData$st_f == "ALABAMA" | RawData$st_f == "MISSISSIPPI", 
               select = c("status", "year", "wave", "sub_reg", "st_f", "common", "area_x",
                                   "landing", "land_var", "wgt_ab1", "var_wab1", "miss_fish"))
+MRIP = subset(MRIP, MRIP$area_x == 2)
 
 MRIP_Func = merge(MRIP, FishList, by = "common", all.x = T)
 
@@ -37,4 +38,26 @@ Unwanted_Species = unique(Unwanted$common)
 MRIP_Func = MRIP_Func[!MRIP_Func$common %in% Unwanted_Species, ]
 
 
+# Access Query Chain Steps ------------------------------------------------
+
+#Step 1: filtering records was done above in the subsetting line. Converting total harvest from kg into tonnes.
+
+Rec_Fish_Step1 = MRIP_Func
+
+Rec_Fish_Step1$Harvest_tonnes = Rec_Fish_Step1$wgt_ab1*0.001
+
+#Step 2: Defines areas of fishing areas and sum tonnes of functional group harvest per year.
+# FishedArea = 263,186 km2 (area from LA-MS-AL 3 miles offshore to EEZ (200 miles offshore))
+
+
+Rec_Fish_Step2 = ddply(Rec_Fish_Step1, .(year, FisheriesGrp), summarize,
+                       Annual_Total_Harvest_tonnes = sum(Harvest_tonnes),
+                       FishedArea = 263186
+                       )
+
+#Step 3: Estimate total harvest tonnes/km2 for each functional group
+
+Rec_Fish_Step3 = ddply(Rec_Fish_Step2, .(year, FisheriesGrp), summarize,
+                       Harvest_Tonnes_km2 = Annual_Total_Harvest_tonnes/FishedArea
+                       )
 
